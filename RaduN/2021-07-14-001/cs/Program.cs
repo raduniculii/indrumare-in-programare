35663c39332bacc1ba629b4e7d3c3673b679e60c
Radu Niculii <raduniculii@gmail.com>
2021-08-25 20:31:34 +0300

    Refactoring la program.cs ca sa 'simplificam' jocul... am redus codul din while-ul mare

--- RaduN/2021-07-14-001/cs/Program.cs
+++ RaduN/2021-07-14-001/cs/Program.cs
@@ -5,15 +5,53 @@ namespace test
 {
     class Program
     {
+        const int IX_HOL = 0;
+        const int IX_BAIE = 1;
+        const int IX_DORMITOR = 2;
+        const int IX_SUFRAGERIE = 3;
+        const int IX_BALCON = 4;
+        const int IX_AFARA = 5;
+        const int IX_CHEIE = 6;
+
+        static List<bool> primaDataIn = new List<bool>(){ true, true, true, true, true, true, true };
+        static List<string> primulText = new List<string>(){
+            $@"Esti in holul unei case din care trebuie neaparat sa iesi."
+            , $@"Esti in baie. Aprinzi lumina si vrei sa te speli pe fata.
+Se arde becul si ai ramas in intuneric.
+"
+            , $@"Dormitorul arata sinistru."
+            , $@"Sufrageria arata primitor, desi tu nu ai timp sa stai la TV."
+            , $@"Din balcon se vede afara. Esti la etajul 12."
+            , null
+        };
+        static List<string> textulUrmator = new List<string>(){
+            $@"Esti in hol."
+            , $@"Esti in baie si e bezna."
+            , $@"Esti in dormitor."
+            , $@"Esti in sufragerie."
+            , $@"Esti in balcon."
+            , ""
+            , null
+        };
+        static List<string> textOptiune = new List<string>(){
+              "usa de la hol."
+            , "usa de la baie"
+            , "usa de la dormitor."
+            , "usa de la sufragerie."
+            , "usa de la balcon"
+            , "usa de intrare/iesire din apartament"
+            , "ridica cheia"
+        };
+
         //in C# specificam tipul returnat (in cazul nostru int) si tipul
         //parametrilor, in cazul nostru o lista / un array de string-uri
-        static int oferaOptiuniSiPreiaRaspunsValid(string[] listaDeOptiuni)
+        static int oferaOptiuniSiPreiaRaspunsValid(int[] listaDeOptiuni)
         {
             Console.WriteLine("Ai urmatoarele optiuni:");
             int index = 0;
             while(index < listaDeOptiuni.Length)
             {
-                Console.WriteLine("   [" + (index + 1).ToString() + "] " + listaDeOptiuni[index]);
+                Console.WriteLine("   [" + (index + 1).ToString() + "] " + textOptiune[listaDeOptiuni[index]]);
 
                 index = index + 1;
             }
@@ -37,7 +75,19 @@ namespace test
                 }
             }
 
-            return nr - 1;
+            return listaDeOptiuni[nr - 1];
+        }
+
+        static void AfiseazaMesajCameraSiActualizeazaPrimaData(int indexCamera)
+        {
+            if(primaDataIn[indexCamera]){
+                Console.WriteLine(primulText[indexCamera]);
+
+                primaDataIn[indexCamera] = false;
+            }
+            else {
+                Console.WriteLine(textulUrmator[indexCamera]);
+            }
         }
 
         static void Main(string[] args)
@@ -45,146 +95,86 @@ namespace test
             //in C# putem defini variabilele cu var si apoi sa le dam o valoare
             //si compilatorul "ghiceste" tipul variabilei la fel ca in js sau python
             //dar putem si sa le specificam direct tipul, cum ar fi string, bool, int, s.a.m.d.
-            string pozitieCurenta = "hol";
+            int pozitieCurenta = IX_HOL;
             bool areCheie = false;
 
-            bool primaDataIn_Hol = true;
-            bool primaDataIn_Baie = true;
-            bool primaDataIn_Dormitor = true;
-            bool primaDataIn_Sufragerie = true;
-            bool primaDataIn_Balcon = true;
+            while(pozitieCurenta != IX_AFARA){
+                if(pozitieCurenta == IX_HOL){
+                    AfiseazaMesajCameraSiActualizeazaPrimaData(IX_HOL);
 
-            while(pozitieCurenta != "afara"){
-                if(pozitieCurenta == "hol"){
-                    if(primaDataIn_Hol){
-                        Console.WriteLine($@"Esti in holul unei case din care trebuie neaparat sa iesi.");
-
-                        primaDataIn_Hol = false;
-                    }
-                    else {
-                        Console.WriteLine($@"Esti in hol.");
-                    }
-
-                    int raspunsUtilizator = oferaOptiuniSiPreiaRaspunsValid(new string[]{
-                        "usa de intrare/iesire din apartament"
-                        , "usa de la baie"
-                        , "usa de la dormitor"
-                        , "usa de la sufragerie"
-                    });
-
-                    string[] listaPozitiiUrmatoare = new string[]{
-                        "afara" //0
-                        , "baie" //1
-                        , "dormitor" //2
-                        , "sufragerie" //3
+                    int[] listaPozitiiUrmatoare = new int[]{
+                        IX_AFARA
+                        , IX_BAIE
+                        , IX_DORMITOR
+                        , IX_SUFRAGERIE
                     };
+                    int raspunsUtilizator = oferaOptiuniSiPreiaRaspunsValid(listaPozitiiUrmatoare);
 
-                    pozitieCurenta = listaPozitiiUrmatoare[raspunsUtilizator];
+                    pozitieCurenta = raspunsUtilizator;
                     
                     //cum areCheie e boolean (adica true sau false), am inlocuit areCheie != "da" cu (nu areCheie, care in limbaj e !areCheie)
-                    if(pozitieCurenta == "afara" && !areCheie)
+                    if(pozitieCurenta == IX_AFARA && !areCheie)
                     {
                         Console.WriteLine("Usa de intrare/iesire din apartament e incuiata si tu nu ai cheie...");
                         Console.WriteLine("Va trebui sa incerci altceva.");
-                        pozitieCurenta = "hol";
+                        pozitieCurenta = IX_HOL;
                     }
                 }
-                else if(pozitieCurenta == "baie"){
-                    if(primaDataIn_Baie){
-                        Console.WriteLine($@"Esti in baie. Aprinzi lumina si vrei sa te speli pe fata.
-            Se arde becul si ai ramas in intuneric.
-            ");
-
-                        primaDataIn_Baie = false;
-                    }
-                    else {
-                        Console.WriteLine($@"Esti in baie si e bezna.");
-                    }
+                else if(pozitieCurenta == IX_BAIE){
+                    AfiseazaMesajCameraSiActualizeazaPrimaData(IX_BAIE);
 
                     Console.WriteLine($@"Nu ai nici o alta optiune decat sa te intorci in hol.");
-                    pozitieCurenta = "hol";
+                    pozitieCurenta = IX_HOL;
                 }
-                else if(pozitieCurenta == "dormitor"){
-                    if(primaDataIn_Dormitor){
-                        Console.WriteLine($@"Dormitorul arata sinistru.");
+                else if(pozitieCurenta == IX_DORMITOR){
+                    AfiseazaMesajCameraSiActualizeazaPrimaData(IX_DORMITOR);
 
-                        primaDataIn_Dormitor = false;
-                    }
-                    else {
-                        Console.WriteLine($@"Esti in dormitor.");
-                    }
-
-                    int raspunsUtilizator = oferaOptiuniSiPreiaRaspunsValid(new string[]{
-                        "usa catre hol"
-                        , "usa catre balcon"
-                    });
-
-                    string[] listaPozitiiUrmatoare = new string[]{
-                        "hol" //0
-                        , "balcon" //1
+                    int[] listaPozitiiUrmatoare = new int[]{
+                        IX_HOL
+                        , IX_BALCON
                     };
+                    int raspunsUtilizator = oferaOptiuniSiPreiaRaspunsValid(listaPozitiiUrmatoare);
 
-                    pozitieCurenta = listaPozitiiUrmatoare[raspunsUtilizator];
+                    pozitieCurenta = raspunsUtilizator;
                 }
-                else if(pozitieCurenta == "sufragerie"){
-                    if(primaDataIn_Sufragerie){
-                        Console.WriteLine($@"Sufrageria arata primitor, desi tu nu ai timp sa stai la TV.");
+                else if(pozitieCurenta == IX_SUFRAGERIE){
+                    AfiseazaMesajCameraSiActualizeazaPrimaData(IX_SUFRAGERIE);
 
-                        primaDataIn_Sufragerie = false;
-                    }
-                    else {
-                        Console.WriteLine($@"Esti in sufragerie.");
-                    }
-
-                    int raspunsUtilizator = oferaOptiuniSiPreiaRaspunsValid(new string[]{
-                        "usa catre hol"
-                        , "usa catre balcon"
-                    });
-
-                    string[] listaPozitiiUrmatoare = new string[]{
-                        "hol" //0
-                        , "balcon" //1
+                    int[] listaPozitiiUrmatoare = new int[]{
+                        IX_HOL
+                        , IX_BALCON
                     };
+                    int raspunsUtilizator = oferaOptiuniSiPreiaRaspunsValid(listaPozitiiUrmatoare);
 
-                    pozitieCurenta = listaPozitiiUrmatoare[raspunsUtilizator];
+                    pozitieCurenta = raspunsUtilizator;
                 }
-                else if(pozitieCurenta == "balcon"){
-                    if(primaDataIn_Balcon){
-                        Console.WriteLine($@"Din balcon se vede afara. Esti la etajul 12.");
-
-                        primaDataIn_Balcon = false;
-                    }
-                    else {
-                        Console.WriteLine($@"Esti in balcon.");
-                    }
+                else if(pozitieCurenta == IX_BALCON){
+                    AfiseazaMesajCameraSiActualizeazaPrimaData(IX_BALCON);
 
                     int raspunsUtilizator = 0;
                     
                     if(areCheie){
-                        raspunsUtilizator = oferaOptiuniSiPreiaRaspunsValid(new string[]{
-                            "usa catre dormitor"
-                            , "usa catre sufragerie"
-                        });
+                        int[] listaPozitiiUrmatoare = new int[]{
+                            IX_BALCON
+                            , IX_SUFRAGERIE
+                        };
+                        raspunsUtilizator = oferaOptiuniSiPreiaRaspunsValid(listaPozitiiUrmatoare);
                     }
                     else {
-                        raspunsUtilizator = oferaOptiuniSiPreiaRaspunsValid(new string[]{
-                            "usa catre dormitor"
-                            , "usa catre sufragerie"
-                            , "ridica cheia de pe jos"
-                        });
+                        int[] listaPozitiiUrmatoare = new int[]{
+                            IX_BALCON
+                            , IX_SUFRAGERIE
+                            , IX_CHEIE
+                        };
+                        raspunsUtilizator = oferaOptiuniSiPreiaRaspunsValid(listaPozitiiUrmatoare);
                     }
 
-                    string[] listaPozitiiUrmatoare = new string[]{
-                        "dormitor" //0
-                        , "sufragerie" //1
-                        , "balcon" //2
-                    };
+                    pozitieCurenta = raspunsUtilizator;
 
-                    pozitieCurenta = listaPozitiiUrmatoare[raspunsUtilizator];
-
-                    if(pozitieCurenta == "balcon"){
+                    if(pozitieCurenta == IX_CHEIE){
                         Console.WriteLine("Ai luat cheia, acum poti deschide ceva.");
                         areCheie = true;
+                        pozitieCurenta = IX_BALCON;
                     }
                 }
             }
