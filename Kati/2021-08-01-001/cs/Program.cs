bf890f00e7b6e1c869f7668499babcb8024b6aa1
Kati-Gyarfas <88103168+Kati-Gyarfas@users.noreply.github.com>
2021-08-28 16:46:58 +0300

    test pentru  revert

--- Kati/2021-08-01-001/cs/Program.cs
+++ Kati/2021-08-01-001/cs/Program.cs
@@ -7,6 +7,8 @@ namespace test
         static void Main(string[] args)
         {
             Console.WriteLine("C# merge!");
+            Console.WriteLine("C# merge!");
+            Console.WriteLine("C# merge!");
         }
     }
 }
