e88103a963df0e7c6020f4abeb6a05b56d790342
Stefan David <davstefan@yahoo.com>
2021-11-16 13:16:22 +0200

    Update numere.py

--- Stefan/2021-11-16-001/python/numere.py
+++ Stefan/2021-11-16-001/python/numere.py
@@ -1,32 +1,7 @@
-# import sys
-
-# numar = int(sys.argv[1])
-
-# unu_zece = ["zero", "unu", "doi", "trei", "patru", "cinci", "sase", "sapte", "opt", "noua"]
-# unsprezece_nouasprezece=["unsprezece", "doisprezece", "treisprezece", "paisprezece", "cincisprezece", "șaisprezece", "șaptesprezece", "optsprezece", "nouăsprezece"]
-# douazeci_nouazeci=["douazeci", "treizeci", "patruzeci", "cincizeci", "saizeci", "saptezeci", "optzeci", "nouazeci"]
-
-# if numar < 10 and numar >= 0:
-#     print(unu_zece[numar])
 
 # #https://numar-text.calculators.ro/numerele-cardinale-reguli-de-scriere-din-cifre-in-cuvinte-cu-litere.php
 
-# print(numar // 10)
-# print(numar // 100)
-# print(numar // 1000)
 
-# print((numar // 1 ) % 10)
-# print((numar // 10 ) % 10)
-# print((numar // 100 ) % 10)
-# print((numar // 1000 ) % 10)
-# print((numar // 10000 ) % 10)
-
-# print("doisprezece mii trei sute patruzeci si cinci")
-# nr = 123
-# l = []
-# l.append((nr // 1 ) % 10)
-# l.append((nr // 10 ) % 10)
-# print(len(l))
 import sys
 
 numar = int(sys.argv[1])
