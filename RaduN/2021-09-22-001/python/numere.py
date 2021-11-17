1c9026a87ace802e16905752e3fb29844c65648e
Radu Niculii <raduniculii@gmail.com>
2021-11-17 19:47:01 +0200

    read si write file

--- RaduN/2021-09-22-001/python/numere.py
+++ RaduN/2021-09-22-001/python/numere.py
@@ -1,27 +1,38 @@
 import sys
 
-numar = int(sys.argv[1])
+#citire argument de la consola... e.g: "py numere.py 12345" fara ghilimele
+#numar = int(sys.argv[1])
 
 listaCifre = ["zero", "unu", "doi", "trei", "patru", "cinci", "sase", "sapte", "opt", "noua"]
 
-if numar < 10 and numar >= 0:
-    print(listaCifre[numar])
-
 #https://numar-text.calculators.ro/numerele-cardinale-reguli-de-scriere-din-cifre-in-cuvinte-cu-litere.php
-
-print(numar // 10)
-print(numar // 100)
-print(numar // 1000)
-
-print((numar // 1 ) % 10)
-print((numar // 10 ) % 10)
-print((numar // 100 ) % 10)
-print((numar // 1000 ) % 10)
-print((numar // 10000 ) % 10)
-
 #print("doisprezece mii trei sute patruzeci si cinci")
-nr = 123
-l = []
-l.append((nr // 1 ) % 10)
-l.append((nr // 10 ) % 10)
-print(len(l))
+
+
+import os 
+# calea catre directorul in care se afla fisierul py
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+
+#writing files
+#deschidem fisierul denumit myTextFile.txt in acelasi fisier cu py-ul nostru
+with open(os.path.join(dir_path, 'myTextFile.txt'), 'w') as writer:
+    # Write the dog breeds to the file in reversed order
+    for i in range(1, 10):
+        writer.write(str(i) + "\n")
+
+# Open a file: file
+with open(os.path.join(dir_path, 'myTextFile.txt'),mode='r') as file:
+    # read all lines at once
+    all_of_it = file.read()
+
+    print(all_of_it)
+
+# Open the file a second time
+with open(os.path.join(dir_path, 'myTextFile.txt'),mode='r') as file:
+    line = file.readline()
+    while line != "":
+        print("linia:")
+        print(line)
+        line = file.readline()
+
