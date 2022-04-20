80dedbad07a948167e0f660ad67256d7189bc7a8
rorcio <raluca_popovici@waters.com>
2022-04-20 20:11:27 +0300

    Program Camere - sesiunea a2a

--- Raluca/Programe/2022-04-13 - Program Camere - Cu Clase/Camere/Program.cs
+++ Raluca/Programe/2022-04-13 - Program Camere - Cu Clase/Camere/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Camere
 {
@@ -6,32 +8,103 @@ namespace Camere
     {
         static void Main(string[] args)
         {
-            var Sufragerie = new Camera(){
+            var AreCheie = false;
+
+            Camera CameraCurenta = null, Baie = null, Hol = null, Dormitor = null, Balcon = null, Sufragerie = null;
+         
+
+             Sufragerie = new Camera(){
                 Mesaj = "Esti in sufragerie. Totul arata foarte straniu. Televizotul este pornit si se vede pe el fata din The Ring",
-                Mesaj2 = "Esti in sufragerie. "
+                Mesaj2 = "Esti in sufragerie. ",
+                Optiuni = new Dictionary<string, Action>() {
+                
+                     {"Mergi in hol", () => {
+                        CameraCurenta = Hol;
+                    }},
+                     {"Mergi in balcon", () => {
+                        CameraCurenta = Balcon;
+                    
+                    }}
+                } 
             };
 
-            var Hol = new Camera(){
+             Hol = new Camera(){
                 Mesaj = "Esti in Hol. Vrei sa iesi afara din casa.",
-                Mesaj2 = "Esti in hol." 
+                Mesaj2 = "Esti in hol.",
+                Optiuni = new Dictionary<string, Action>() {
+                    {"Deschide usa de iesire", () => {
+                        System.Console.WriteLine("Incerci sa deschizi usa");
+                        if (AreCheie){
+                          System.Console.WriteLine("Esti liber, poti iesi");  
+                          CameraCurenta = null;
+                        }
+                        else {
+                            System.Console.WriteLine("E incuiat, tzapa!");
+                        }
+                    }},
+                     {"Mergi in baie", () => {
+                        CameraCurenta = Baie;
+                    }},
+                     {"Mergi in dormitor", () => {
+                        CameraCurenta = Dormitor;
+                    }},
+                     {"Mergi in sufragerie", () => {
+                        CameraCurenta = Sufragerie;
+                    
+                    }}
+
+                    
+                } 
             }; 
 
-            var Dormitor = new Camera(){
+             Dormitor = new Camera(){
                 Mesaj = "Esti in Dormitor. Patul este nefacut si sunt urme de sange pe podea",
-                Mesaj2 = "Esti in Dormitor." 
+                Mesaj2 = "Esti in Dormitor." ,
+                Optiuni = new Dictionary<string, Action>() {
+                
+                     {"Mergi in hol", () => {
+                        CameraCurenta = Hol;
+                    }},
+                     {"Mergi in balcon", () => {
+                        CameraCurenta = Balcon;
+                    
+                    }}
+                } 
             }; 
 
-            var Baie = new Camera(){
+             Baie = new Camera(){
                 Mesaj = "Esti in Baie. Becul este stins si nu vezi nimic",
-                Mesaj2 = "Esti in Baie. Becul este ars. Iesi afara ca nu vezi nimic." 
+                Mesaj2 = "Esti in Baie. Becul este ars. Iesi afara ca nu vezi nimic." ,
+                Optiuni = new Dictionary<string, Action>() {
+                
+                     {"Iesi inapoi in hol", () => {
+                        CameraCurenta = Hol;
+                    }}
+                } 
             }; 
             
-            var Balcon = new Camera(){
+             Balcon = new Camera(){
                 Mesaj = "Esti in Balcon. Vrei sa te uiti pe geam dar nu vezi nimic ",
-                Mesaj2 = "Esti in Balcon." 
+                Mesaj2 = "Esti in Balcon." ,
+                Optiuni = new Dictionary<string, Action>() {
+                
+                     {"Mergi in dormitor", () => {
+                        CameraCurenta = Dormitor;
+                    }},
+                     {"Mergi in sufragerie", () => {
+                        CameraCurenta = Sufragerie;
+                    
+                    }},
+                    
+                     {"Ridica cheia", () => {
+                        AreCheie = true;
+                        System.Console.WriteLine("Bravo, ai gasit cheia");
+                        Balcon.Optiuni.Remove("Ridica cheia");
+                    }}
+                } 
             }; 
 
-            var CameraCurenta = Hol;
+            CameraCurenta = Hol;
 
             var UsaIesire = new Usa()
             {
@@ -67,7 +140,36 @@ namespace Camere
                 CameraB = Sufragerie
             };
 
-            Console.WriteLine(CameraCurenta.Mesaj);
+            string RaspunsulUtilizatorului = "";
+
+            int Raspuns = 0;
+
+            while (CameraCurenta != null)
+            {
+                // Afisam optiunile si una dintre optiuni trebuie sa fie iesirea
+                Console.WriteLine(CameraCurenta.Mesaj);
+                Console.WriteLine("Ai urmatoarele optiuni, ce alegi?");
+                int i = 1;
+                foreach (var item in CameraCurenta.Optiuni)
+                {
+                    Console.WriteLine(i.ToString() + " " + item.Key);
+                    i+=1;
+                }
+                Console.WriteLine($"{i} Iesi din Joc");
+                RaspunsulUtilizatorului = Console.ReadLine();
+                while ( !(int.TryParse(RaspunsulUtilizatorului, out Raspuns) && Raspuns >=1 && Raspuns <= i) )
+                {
+                    Console.WriteLine($"Alege un numar intre 1 si {i}");
+                    RaspunsulUtilizatorului = Console.ReadLine();
+
+                }
+                Raspuns = int.Parse(RaspunsulUtilizatorului);
+                if (Raspuns == i) break;
+
+                CameraCurenta.Optiuni.ElementAt(Raspuns - 1).Value();
+            }
+
+            
 
 
             }
