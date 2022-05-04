ba40aa798a09582040217a554799764a4fd8bd4b
Kati-Gyarfas <88103168+Kati-Gyarfas@users.noreply.github.com>
2022-05-04 20:15:49 +0300

    Joc Carti

---
+++ Kati/JocCarti/Joc2/CarteJoc.cs
@@ -0,0 +1,8 @@
+namespace Joc2
+{
+    class CarteJoc 
+    {
+        public int Valoare { get; set; }
+        public string Culoare { get; set; }
+    }
+}
---
+++ Kati/JocCarti/Joc2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Joc2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pachetCarti = new List<CarteJoc>();
+            var culori = new List<string>()
+            {
+                "inima rosie", "romb", "trefla", "inima neagra"
+            };
+            foreach (var culoare in culori)
+            {
+                for (int i = 2; i < 15; i++)
+                {
+                    var carteNoua = new CarteJoc(){ 
+                        Culoare = culoare, Valoare = i
+                    };
+
+                    pachetCarti.Add(carteNoua);
+                }    
+            }
+            System.Console.WriteLine("Cartile de joc sunt:");
+
+            foreach (var carte in pachetCarti)
+            {
+                System.Console.WriteLine($"{carte.Valoare} de {carte.Culoare}");
+            }
+
+        }
+    }
+}
