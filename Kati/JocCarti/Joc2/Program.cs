942c71a8c3ba84e83f23fb52c94b2f6736ef3428
Kati-Gyarfas <88103168+Kati-Gyarfas@users.noreply.github.com>
2022-05-19 09:29:18 +0300

    Update curs 05.18

--- Kati/JocCarti/Joc2/Program.cs
+++ Kati/JocCarti/Joc2/Program.cs
@@ -67,8 +67,77 @@ namespace Joc2
 
             Random rnd = new Random();
 
-            var pachetAmestecat = pachetCarti.OrderBy(c => rnd.Next());
+            var pachetAmestecat = pachetCarti.OrderBy(c => rnd.Next()).ToList();
 
+            var manaJucator = new List<CarteJoc>();
+            var n = 5;
+
+
+            manaJucator = pachetAmestecat.GetRange(0 , n);
+            pachetAmestecat.RemoveRange(0, n);
+
+            System.Console.WriteLine("Cartile jucatorului sunt:");
+
+            foreach (var carte in manaJucator)
+            {
+                System.Console.Write($"{carte.Nume}");
+                if (carte.Culoare == "inima rosie")
+                {
+                    arataInima();
+                }
+                else if (carte.Culoare == "romb")
+                {
+                    arataRomb();
+                }
+                else if (carte.Culoare == "trefla")
+                {
+                    arataTrefla();
+                }    
+                else 
+                {
+                    arataNeagra();
+                }        
+                System.Console.WriteLine();
+            }
+
+            System.Console.WriteLine("Spune pozitia cartilor pe care le arunci?");
+            string cartiAruncate = Console.ReadLine();
+
+            var impartite = cartiAruncate.Split(" ");
+
+
+            foreach (var pozitie in impartite)
+            {
+
+                manaJucator[int.Parse(pozitie)-1] = pachetAmestecat[0];
+                pachetAmestecat.RemoveAt(0);
+
+            }
+            System.Console.WriteLine("Noile carti sunt:");
+
+            foreach (var carte in manaJucator)
+            {
+                System.Console.Write($"{carte.Nume}");
+                if (carte.Culoare == "inima rosie")
+                {
+                    arataInima();
+                }
+                else if (carte.Culoare == "romb")
+                {
+                    arataRomb();
+                }
+                else if (carte.Culoare == "trefla")
+                {
+                    arataTrefla();
+                }    
+                else 
+                {
+                    arataNeagra();
+                }        
+                System.Console.WriteLine();
+            }
+
+/*
             System.Console.WriteLine("Cartile de joc sunt:");
 
             foreach (var carte in pachetAmestecat)
@@ -89,9 +158,9 @@ namespace Joc2
                 else 
                 {
                     arataNeagra();
-                }            
+                }        
                 System.Console.WriteLine();
-            }
+            }*/
         }
 
     }
@@ -104,5 +173,24 @@ namespace Joc2
 1. Nume carti - valet, popa, as
 2. Caractere ASCII
 3. Amestecat pachetul de carti
+4. creat copie a pachetului de carti, luati fiecare carte una cate una random si sa le puneti in celalalt pachet.  
+5. afiseaza primele 5 carti
+6. mecanism pentru hold
+
+7. de facut n-ul
+8. calculare puncte 
+
+https://www.casinoreports.ca/video-poker/rules/
+
+Coins	1	2	3	4	5
+Royal Flush	250	500	750	1000	4000
+Straight Flush	50	100	150	200	250
+Four of a Kind	25	50	75	100	125
+Full House	9	18	27	36	45
+Flush	6	12	18	24	30
+Straight	4	8	12	16	20
+Three of a Kind	3	6	9	12	15
+Two Pair	2	4	6	8	10
+Jacks or Better	1	2	3	4	5
 
 */
